4bcd2737909a9ee35e3b8a8b22c8f58216cdec2b
renyh <renyh1013@163.com>
2017-08-12 17:51:04 +0800

    增加第一个Web MVC程序

---
+++ firstMvcApplication/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstMvcApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]//登陆控制器
+        public ActionResult Login(Models.LoginModel loginModel)
+        {
+            string info = "";
+            if (loginModel.UserName == "张三" && loginModel.Password == "123456")
+                info="恭喜您，登录成功！";
+            else
+                info = "<span style='color:red'>登录失败：用户名或密码不正确！</span>";
+
+            ViewBag.LoginInfo = info;
+            return View();
+        }
+
+    }
+}
---
+++ firstMvcApplication/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace firstMvcApplication.Models
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }            //用户名
+        public string Password { get; set; }            //密码
+    }
+
+}
