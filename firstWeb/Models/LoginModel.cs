996224f3ce81d5d8039a68e01b17149095dc6e4c
renyh <renyh1013@163.com>
2017-08-13 14:36:24 +0800

    重新创建web示例程序

---
+++ firstWeb/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace firstWeb.Models
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }            //用户名
+        public string Password { get; set; }            //密码
+    }
+}
