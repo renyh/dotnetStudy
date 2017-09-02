6feac618e734bc50d7a56e0c3ef42864de1d021e
renyh <renyh1013@163.com>
2017-09-02 17:05:53 +0800

    add CS08

---
+++ CS08/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 程序调试
+// 1）设置断点 
+// 2）逐步执行 F11 
+// 3）观察变量
+namespace CS08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 局部变量定义
+            int a = 1;
+
+            // while 循环执行
+            while (a <= 3)
+            {
+                Console.WriteLine("a 的值： {0}", a);
+                a++;
+            }
+
+            Console.WriteLine("=end=");
+        }
+    }
+}
